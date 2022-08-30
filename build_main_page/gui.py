cdb6fbb9c861a861a6329215d1d2b5b7d9cc4a7f
shanishilko <shanishilko@gmail.com>
2022-08-30 14:00:02 +0300

    add load data set win

--- build_main_page/gui.py
+++ build_main_page/gui.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # from tkinter import *
 # Explicit imports to satisfy Flake8
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
-import build2.gui as next_win
+import build_load_dataset.gui as next_win
 
 OUTPUT_PATH = Path(__file__).parent
 ASSETS_PATH = OUTPUT_PATH / Path("./assets")
